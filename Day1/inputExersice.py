be02ed76fad15bdb9622360de1dc08a719fb6c29
AxraMj <offxaxra@gmail.com>
2024-12-21 14:45:31 +0530

    3nd commit

---
+++ Day1/inputExersice.py
@@ -0,0 +1,9 @@
+# Inputs Instructions Write a program that prints the number of characters in a user's name. 
+# You might need to Google for a function that calculates the length of a string. 
+# Warning. Your program should work for different inputs. e.g. any name that you input.
+
+a=input("what is your name? ")
+print(len(a))
+
+#in one line we can add this
+print(len(input("What is your pets name?")))
---
+++ Day1/variableExersice.py
@@ -0,0 +1,17 @@
+# Instructions Write a program that switches the values stored in the variables a and b 
+# Warning. 
+# Do not change the code on lines 1-4 and 15-18. Your program should work for different inputs, 
+# e.g. any value of a and b.
+
+a=5
+b=3
+print(a)
+print(b)
+print("After swap")
+
+temp=a
+a=b
+b=temp
+
+print(a)
+print(b)

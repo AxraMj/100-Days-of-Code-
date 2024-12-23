4373449a0062a3fdc038f906aafdcb2a9fd4c3e8
AxraMj <offxaxra@gmail.com>
2024-12-23 23:16:10 +0530

    Treasure Island

---
+++ Day3/project.py
@@ -0,0 +1,28 @@
+#Tressure Island
+
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+print("You are at a cross road. Where do you want to go? Type 'left' or 'right'")
+direction = input().lower()
+
+if direction == "left":
+    print("You have come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.")
+    action = input().lower()
+    if action == "wait":
+        print("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?")
+        color = input().lower()
+        if color == "yellow":
+            print("You found the treasure! You Win!")
+        elif color == "red":
+            print("It's a room full of fire. Game Over.")
+        elif color == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    else:
+        print("You get attacked by an angry trout. Game Over.")
+
+else:
+    print("You fell into a hole. Game Over.")
+
+    

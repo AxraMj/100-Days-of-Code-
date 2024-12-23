96d562d914b8d38816cc71e8d6a3a4f7c605b5af
AxraMj <offxaxra@gmail.com>
2024-12-23 20:43:14 +0530

    15nd commit

---
+++ Day3/exc3.py
@@ -0,0 +1,8 @@
+print("Lets find the Leap year!! ")
+year=int(input("Enter the a year:"))
+
+if year%4 == 0:
+    if year%100==0 and year%400==0 or year%100!=0:
+        print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")

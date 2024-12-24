4a477c42704aea4edbcee27d1ee2879a77154350
AxraMj <offxaxra@gmail.com>
2024-12-24 22:07:59 +0530

    4st update

---
+++ Day4/nestedlist.py
@@ -0,0 +1,10 @@
+#Nested List
+#defining a nested list
+#Nested list is a list within a list
+#Nested list is a list that appears as an element in another list.
+
+male=["arun","akhil","anand","anoop","anil"]
+female=["sneha","anu","anju","anju","anju"]
+
+gender=[male,female] #nested list
+print(gender)

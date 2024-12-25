fbfd4fc2768c03d04a1dc37533ddb3cf28f46a4f
AxraMj <offxaxra@gmail.com>
2024-12-25 15:39:46 +0530

    10th update

--- Day5/ex1.py
+++ Day5/ex1.py
@@ -4,7 +4,14 @@
 # e.g. student scores (78, 85, 85, 86, 55, 91, 54, 891 Important you are not allowed to use the max or min functions.
 #  The output words must match the example. i.e The highest score in the class list x
 
-scores=[78, 85, 86, 55, 91, 54, 891]
+print("Welcome to the Highest Score Calculator")
+score=input("Enter the scores of the students separated by commas: ").split(", ")
+for n in range(0,len(score)):
+    score[n]=int(score[n])
+print(score)
 
-# for score in scores:
-#     if score
+height_score=0
+for maxscore in score:
+    if maxscore > height_score:
+        height_score=maxscore
+print(height_score)

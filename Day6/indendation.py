10b5e6d135c8c6365d5c798048757287671ed469
AxraMj <offxaxra@gmail.com>
2024-12-27 10:51:41 +0530

    day 6

---
+++ Day6/indendation.py
@@ -0,0 +1,9 @@
+#indentation
+#definition: 
+# Indentation refers to the spaces at the beginning of a code line. 
+# Where in other programming languages the indentation in code is for readability only,
+# the indentation in Python is very important. Python uses indentation to indicate a block of code.
+#example:
+# def my_function():
+#     print("hello")
+#     print("Hi")

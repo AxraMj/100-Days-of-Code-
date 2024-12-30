40a63d1751690875d33950f0f9f5ee56657d2328
AxraMj <offxaxra@gmail.com>
2024-12-30 23:04:03 +0530

    Day 9

--- Day9/index.py
+++ Day9/index.py
@@ -1,7 +1,12 @@
 programing_definitions={
     "Bug":"any of various small arthropods (such as a beetle or spider) resembling the true bugs.",
     "Function":"A block of code that",
-    "Loop":"Repete exection of code"
 }
 
 print(programing_definitions["Bug"])
+print()
+
+
+#To add new item into the dictionary
+programing_definitions["Loop"]="Repete exection of code"
+print(programing_definitions)

6522b7f7c24cc9531084cee15e4f92878618d11d
AxraMj <offxaxra@gmail.com>
2024-12-30 22:58:53 +0530

    Day 9

---
+++ Day9/index.py
@@ -0,0 +1,7 @@
+programing_definitions={
+    "Bug":"any of various small arthropods (such as a beetle or spider) resembling the true bugs.",
+    "Function":"A block of code that",
+    "Loop":"Repete exection of code"
+}
+
+print(programing_definitions["Bug"])

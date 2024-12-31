4029cc0bac9194207f9b487dfcc9ab0ebdc79479
AxraMj <offxaxra@gmail.com>
2024-12-31 14:49:08 +0530

    Day 10.5

---
+++ Day10/ex1.py
@@ -0,0 +1,24 @@
+#Check leap year and say  number of days in the month
+
+def is_leap_year(year):
+    if year%4==0:
+        if year%100==0:
+            if year%400==0:
+                return True
+            return False
+        return True
+    return False
+        
+def month_days(year,month):
+    days_in_month=[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap_year(year) and month==2:
+        return 29
+    return days_in_month[month-1]
+
+
+
+
+year=int(input("Enter Year: "))
+month=int(input("Enter the month:"))
+output=month_days(year,month)
+print(output)

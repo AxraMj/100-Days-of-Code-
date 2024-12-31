d9eb15fb7fec23f38b29f27836c347bf6ed96d8e
AxraMj <offxaxra@gmail.com>
2024-12-31 12:23:28 +0530

    Day 10.3

--- Day10/functioPro.py
+++ Day10/functioPro.py
@@ -4,7 +4,8 @@ last_name=input("Enter your last name:")
 def format_name(f_name,l_name):
     formated_f_name=f_name.title()
     formated_l_name=l_name.title()
-    print(f"{formated_f_name} {formated_l_name}")
+    return f"{formated_f_name} {formated_l_name}"
 
 
-format_name(first_name,last_name)
+output=format_name(first_name,last_name)
+print(output)

8c82339878b5f74eebea8056977c541a4f1e4a75
AxraMj <offxaxra@gmail.com>
2025-01-02 11:45:19 +0530

    Day 12

--- Day12/ModifingScope.py
+++ Day12/ModifingScope.py
@@ -13,3 +13,9 @@ def scope():
     return values+1
 output=scope()
 print(output)
+
+#global scope is maily used to modify the value of the variable in the global scope
+#global scope is mainly used for constants like pi=3.14, 
+#                                               url="www.google.com" , 
+#                                               social security number , 
+#                                               social media username etc

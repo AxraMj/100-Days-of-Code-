4b9b55c4d790772b958988f9a80b40b4c6b61daf
AxraMj <offxaxra@gmail.com>
2025-01-02 11:36:07 +0530

    Day 12

--- Day12/ModifingScope.py
+++ Day12/ModifingScope.py
@@ -1,11 +1,15 @@
 #modifing scope of variable
 
 values=5
-
 def scope():
     global values
     values +=1
     print(values)
-
-
 scope()
+
+#simple ways to do the above code
+values=20
+def scope():
+    return values+1
+output=scope()
+print(output)

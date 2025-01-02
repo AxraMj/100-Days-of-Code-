3530101c27fcab98e675f705a221829f5959ab09
AxraMj <offxaxra@gmail.com>
2025-01-02 10:12:07 +0530

    Day 12

---
+++ Day12/scope.py
@@ -0,0 +1,11 @@
+##################### Scope ##############################
+
+a=10 #global scope: outside the function
+
+def sum():
+    b=10 #local scope : inside the function
+    print(a+b)
+
+sum()
+print(a)
+# print(b) # This will throw an error because b is not defined in the global scope

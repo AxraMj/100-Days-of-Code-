faae4e143486347700747fae69db31a6e3c8c0d2
AxraMj <offxaxra@gmail.com>
2025-01-04 20:46:19 +0530

    Day 16 Project

--- Day16/Turtle Project/main.py
+++ Day16/Turtle Project/main.py
@@ -3,6 +3,12 @@ from turtle import Turtle, Screen
 timmy=Turtle() #timmy is object #Turtle is class
 print(timmy)
 timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
 
 my_screen=Screen() #my_screen is object and Screen() is class
 print(my_screen.canvheight) #my_screen is object and canvheight attribute

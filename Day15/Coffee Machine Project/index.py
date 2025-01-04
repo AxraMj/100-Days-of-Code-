a0c924df2388474e6fccb3c91023989ea1c52a41
AxraMj <offxaxra@gmail.com>
2025-01-04 19:32:29 +0530

    Day 15 Project

--- Day15/Coffee Machine Project/index.py
+++ Day15/Coffee Machine Project/index.py
@@ -2,30 +2,32 @@ import Art
 import data
 
 print(Art.logo)
-
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
-repete=True
-while repete:
-    users_choice=input("What would you like? (espresso/latte/cappuccino): ")
-    if users_choice=='report':
+
+start=True
+while start:
+    user_choise=input("Press the Button? Press 'A' for ingredients And 'B' for Menu and 'F' for off:")
+    if user_choise=='A':
         for i in resources:
-            print(i  , resources[i])
-    elif users_choice=='espresso':
-        print("Please insert coins.")
-        Coin_insert=input("cost $1.5 ?: ")
-        if Coin_insert!='1.5':
-            print("Not sufficient money, money refunded")
-    elif users_choice=='latte':
-        print("Please insert coins.")
-        Coin_insert=input("how many ?: ")
-    elif users_choice=='cappuccino':
-        print("Please insert coins.")
-        Coin_insert=input("how many ?: ")
-    else:
-        print("Invalid")
+            print(i,resources[i])
+    elif user_choise=='B':
+        menu_choice = input("​What would you like? (espresso($1.5)/latte($2.5)/cappuccino($3.0)): ")
+        if menu_choice=='espresso' or menu_choice=='latte' or menu_choice=='cappuccino' :
+            coin=input("Please insert coins.")
+            if coin=='$1.5' or coin=='$2.5' or coin=='$3.0':
+                print("Here is your order ☕️. Enjoy!")
+            else:
+                if coin>'$2.5' or coin >'$1.5' or coin>'$3.0':
+                    print("Get balance")
+                    print("Here is your order ☕️. Enjoy!")
+                else:
+                    print("Not sufficient amount to get drink")
+    elif user_choise=='F':
+        start=False
+
 
 

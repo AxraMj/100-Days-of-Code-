1e924a7bd37e5ec4a272a29a2f654db867848a29
AxraMj <offxaxra@gmail.com>
2025-01-04 20:42:24 +0530

    Day 16 Project

--- Day16/Turtle Project/main.py
+++ Day16/Turtle Project/main.py
@@ -1,4 +1,9 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 
-timmy=Turtle()
+timmy=Turtle() #timmy is object #Turtle is class
 print(timmy)
+timmy.shape("turtle")
+
+my_screen=Screen() #my_screen is object and Screen() is class
+print(my_screen.canvheight) #my_screen is object and canvheight attribute
+my_screen.exitonclick() #exitonclick is method

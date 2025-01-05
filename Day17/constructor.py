bd62134a7d4ee45d955f2ccf3284987018d39712
AxraMj <offxaxra@gmail.com>
2025-01-05 22:43:36 +0530

    Day 17

---
+++ Day17/constructor.py
@@ -0,0 +1,9 @@
+class users:
+    def __init__(self,user_id,user_name):
+        self.user_id=user_id
+        self.user_name=user_name
+
+user1=users(1,"Akshara")
+user2=users(2,"Avi")
+
+print(user1.user_id)

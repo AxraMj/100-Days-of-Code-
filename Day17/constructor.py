8634dad57f9fb10cd2a4f0c58e03f09a93ae22c9
AxraMj <offxaxra@gmail.com>
2025-01-05 22:50:43 +0530

    Day 17

--- Day17/constructor.py
+++ Day17/constructor.py
@@ -2,8 +2,17 @@ class users:
     def __init__(self,user_id,user_name):
         self.user_id=user_id
         self.user_name=user_name
+        self.follower=0
+        self.following=0
+    def perform_follow(self,user):
+        user.follower+=1
+        self.following+=1
 
 user1=users(1,"Akshara")
 user2=users(2,"Avi")
 
-print(user1.user_id)
+user1.perform_follow(user2)
+print(user1.follower)
+print(user1.following)
+print(user2.follower)
+print(user2.following)

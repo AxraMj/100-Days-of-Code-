940396f2c83736657d440a13520df4e5aa6d4ed7
AxraMj <offxaxra@gmail.com>
2025-01-05 22:32:02 +0530

    Day 17

--- Day17/index.py
+++ Day17/index.py
@@ -5,3 +5,9 @@ user1=users()
 user1.name="Akshara"
 user1.age=22
 print(user1.name)
+
+#if we have many object to create it will be very difficult to do so like this
+user2=users()
+user2.name="aB"
+user2.age=22
+print(user2.name)

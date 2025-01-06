2119168dbbb55e124f73e30932b5360b041ffedd
AxraMj <offxaxra@gmail.com>
2025-01-06 15:19:33 +0530

    Day 17 Quiz Project

---
+++ Day17/project/question_model.py
@@ -0,0 +1,9 @@
+class Question:
+    def __init__(self,text,answer):
+        self.text=text
+        self.answer=answer
+
+
+#obj1=Question("Austarlian flag is yellowin color","true")
+#obj2=Question("brazil flag is yellowin color","true")
+#instead of writing like this we can do another simple method

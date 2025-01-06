ce83bf834d6a56327180c6aba9a51950c2827c71
AxraMj <offxaxra@gmail.com>
2025-01-06 22:54:00 +0530

    Day 18

--- Day18/challenge4.py
+++ Day18/challenge4.py
@@ -4,8 +4,15 @@ tim=t.Turtle()
 directions=[0,90,180,270]
 tim.pensize(15)
 tim.speed("fastest")
-colours=["green","yellow","beige","bisque4","BlueViolet","burlywood4","coral2","cornsilk","crymson","DarkGoldenrod2","DarkOliveGreen4","DeepPink4","firebrick1","gray10"]
+t.colormode(255)
+
+def random_color():
+    r=random.randint(0,255)
+    g=random.randint(0,255)
+    b=random.randint(0,255)
+    random_color=(r,g,b)
+    return random_color
 for _ in range(200):
-    tim.color(random.choice(colours))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))

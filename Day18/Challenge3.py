f4d95e93e16b3207492b2b2e445d6fbb29fa78ce
AxraMj <offxaxra@gmail.com>
2025-01-06 22:19:24 +0530

    Day 18

--- Day18/Challenge3.py
+++ Day18/Challenge3.py
@@ -3,9 +3,9 @@ tt_turtle_obj = Turtle()
 
 for _ in range(15):
     tt_turtle_obj.forward(10)
-    tt_turtle_obj.color("white")
+    tt_turtle_obj.penup()
     tt_turtle_obj.forward(10)
-    tt_turtle_obj.color("black")
+    tt_turtle_obj.pendown()
 
 screen = Screen()
 screen.exitonclick()

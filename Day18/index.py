376023887da8b276fbc154abdb93d5572b4b11fc
AxraMj <offxaxra@gmail.com>
2025-01-06 21:01:09 +0530

    Day 18

--- Day18/index.py
+++ Day18/index.py
@@ -1,18 +1,12 @@
 from turtle import *
 
 timmy_the_turtle=Turtle()
-timmy_the_turtle.shape("turtle")
+
 
 timmy_the_turtle.color("khaki4")
-timmy_the_turtle.forward(25)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(25)
-
-
-
-
-
-
+for _ in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
 
 
 

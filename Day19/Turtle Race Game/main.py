5c692dbd63bcd411b1d9501d6199d58c6673c22b
AxraMj <offxaxra@gmail.com>
2025-01-07 13:34:21 +0530

    Day 19 Turtle race

--- Day19/Turtle Race Game/main.py
+++ Day19/Turtle Race Game/main.py
@@ -1,20 +1,15 @@
 from turtle import *
-
-
 screen=Screen()
 screen.setup(width=500,height=400)
 screen.textinput(title="Lets Bet",prompt="Who will win the Race?Guess Color")
 
-color=["red","green","blue","yellow","orange"]
+color=["red","green","blue","yellow","orange","purple"]
 y_position=[-70,-40,-10,20,50,80]
 
 for turtle_index in range(0,6):
     tim=Turtle(shape="turtle")
+    tim.color(color[turtle_index])
     tim.penup()
     tim.goto(x=-230,y=y_position[turtle_index])
-
-
-
-
 
 screen.exitonclick()

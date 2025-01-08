b1cc1229a26dd53ccdd6858cd87142131c89544f
AxraMj <offxaxra@gmail.com>
2025-01-08 22:56:39 +0530

    Day 21

---
+++ Day21/slicing.py
@@ -0,0 +1,11 @@
+list=["a","b","c","d","e","f","g","h","i","j","k","l","m"]
+
+print(list[2:5])
+print(list[:7])
+print(list[5:])
+print(list[2:8:2])
+print(list[::2])
+print(list[5:10])
+print(list[::-1])
+
+#slicing

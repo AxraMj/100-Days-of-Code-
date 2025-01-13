f22f391d916704cce1b8a630009329270eb075d8
AxraMj <offxaxra@gmail.com>
2025-01-13 21:25:03 +0530

    Day 24

--- Day24/index.py
+++ Day24/index.py
@@ -1,3 +1,12 @@
 file = open("C:/Users/HP/100-Days-of-Code-/Day24/my_file.txt")
 content =file.read()
 print(content)
+file.close() #to free up resource
+
+#or
+with open("C:/Users/HP/100-Days-of-Code-/Day24/my_file.txt") as file:
+    content = file.read()
+    print(content)
+
+#by reading file with 'with' keyword then we no need
+#to use close() because, it vertualy do the closing() functionality without manually writing

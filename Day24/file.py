1b467692537c92c0681cb8218b77a7d3bc25b3b9
AxraMj <offxaxra@gmail.com>
2025-01-13 21:31:30 +0530

    Day 24

---
+++ Day24/file.py
@@ -0,0 +1,8 @@
+with open("C:/Users/HP/100-Days-of-Code-/Day24/my_file.txt",mode="w") as file:
+    file.write("New text to write")
+
+#without deleting the content inside the text file add content then we need to use mode="a" 
+#which means append
+
+with open("C:/Users/HP/100-Days-of-Code-/Day24/my_file.txt",mode="a") as file:
+    file.write("\nadd this also")

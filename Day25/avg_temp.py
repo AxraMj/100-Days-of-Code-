de32c210daa341fc0e38c44211be1348cb78bbd4
AxraMj <offxaxra@gmail.com>
2025-01-14 11:18:49 +0530

    Day 25 pandas

--- Day25/avg_temp.py
+++ Day25/avg_temp.py
@@ -4,4 +4,11 @@ weather_file_path="C:/Users/HP/100-Days-of-Code-/Day25/002 weather-data.csv"
 data=pandas.read_csv(weather_file_path)
 list_data=data["temp"].to_list()
 avg=sum(list_data)/len(data["temp"])
-print(avg)
+print(avg) #output 17.428571428571427
+
+#this is how we normaly calculate the avg but,there is a easy method in pandas
+#ie,
+
+print(data["temp"].mean()) #output 17.428571428571427
+#to find max from temperature
+print(data["temp"].max()) #output 24 

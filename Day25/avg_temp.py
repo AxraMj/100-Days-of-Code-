ecff3a219ed51325ed60ceddb1e36651481449e4
AxraMj <offxaxra@gmail.com>
2025-01-14 12:09:45 +0530

    Day 25 pandas

--- Day25/avg_temp.py
+++ Day25/avg_temp.py
@@ -12,3 +12,23 @@ print(avg) #output 17.428571428571427
 print(data["temp"].mean()) #output 17.428571428571427
 #to find max from temperature
 print(data["temp"].max()) #output 24 
+
+#also we can print temp like this
+print(data.temp)
+#output
+# 0    12
+# 1    14
+# 2    15
+# 3    14
+# 4    21
+# 5    22
+# 6    24
+print(data.condition)
+#output
+# 0     Sunny
+# 1      Rain
+# 2      Rain
+# 3    Cloudy
+# 4     Sunny
+# 5     Sunny
+# 6     Sunny

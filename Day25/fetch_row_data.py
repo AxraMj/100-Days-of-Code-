368995b8274505caac3d43e57181e1f72c8b5727
AxraMj <offxaxra@gmail.com>
2025-01-14 12:27:32 +0530

    Day 25 pandas

---
+++ Day25/fetch_row_data.py
@@ -0,0 +1,21 @@
+import pandas
+
+weather_file_path="C:/Users/HP/100-Days-of-Code-/Day25/002 weather-data.csv"
+data=pandas.read_csv(weather_file_path)
+
+fetch_row=data[data.day == "Monday"]
+print(fetch_row)
+
+#output
+#       day  temp condition
+# 0  Monday    12     Sunny
+
+#fetch row that is having max temperature
+max_temp=data.temp.max()
+fetch_max_temp=data[data.temp == max_temp]
+print(fetch_max_temp)
+#writing the above code in one line is:
+print(data[data.temp == data.temp.max()])
+#output 
+# day  temp condition
+# 6  Sunday    24     Sunny

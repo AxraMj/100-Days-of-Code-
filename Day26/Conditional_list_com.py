26b97e5aa6c5a370ccf1d480e91c4fea46ec4050
AxraMj <offxaxra@gmail.com>
2025-01-15 22:40:06 +0530

    Day 26

---
+++ Day26/Conditional_list_com.py
@@ -0,0 +1,6 @@
+#conditional list comprehension
+#syntax
+# new_list=[new_item for item in list if test]
+names=["Akshara","Akash","Avinash","Aiswarya"]
+short_name=[name for name in names if len(name)< 7]
+print(short_name)
---
+++ Day26/list_com3.py
@@ -0,0 +1,3 @@
+
+new_list=[letter*2 for letter in range(1,5)]
+print(new_list)

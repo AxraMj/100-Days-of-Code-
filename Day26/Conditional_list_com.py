1cb2637122f73bd93415e35b378ce7ac7a1e9acb
AxraMj <offxaxra@gmail.com>
2025-01-15 22:46:10 +0530

    Day 26

--- Day26/Conditional_list_com.py
+++ Day26/Conditional_list_com.py
@@ -2,5 +2,5 @@
 #syntax
 # new_list=[new_item for item in list if test]
 names=["Akshara","Akash","Avinash","Aiswarya"]
-short_name=[name for name in names if len(name)< 7]
+short_name=[name.upper() for name in names if len(name)< 7]
 print(short_name)

e0f2fdd46c7aef216fbba462100ff84847d82c0b
AxraMj <offxaxra@gmail.com>
2025-01-15 22:09:03 +0530

    Day 26

---
+++ Day26/List_Comprehension.py
@@ -0,0 +1,12 @@
+number = [1,2,3]
+new_list=[]
+for n in number:
+    add_1=n+1
+    new_list.append(add_1)
+    
+
+#This how we loop through list in usual case but,
+#using list comprehension we can use it in oneline
+
+#syntax
+# new_list=[new_item for item in list]

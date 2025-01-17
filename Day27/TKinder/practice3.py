2eb86b2bfb3d6376ef7eaaf85e006953ad3a096b
AxraMj <offxaxra@gmail.com>
2025-01-17 13:10:24 +0530

    Day 27 TKinder Entry

--- Day27/TKinder/practice3.py
+++ Day27/TKinder/practice3.py
@@ -7,15 +7,18 @@ window.minsize(width=500,height=300)
 my_label=Label(text="Click me")
 my_label.pack()
 
+#Entry (to input something)
+input_text= Entry()
+input_text.pack()
+
+
 def click_me():
-    my_label["text"]="oh!! I am Clicked"
+    my_label["text"]=input_text.get()
     
 
 my_button=Button(text="Click me", command=click_me)
 my_button.pack()
 
-#Entry (to input something)
-input_text= Entry()
-input_text.pack()
+
 
 window.mainloop()

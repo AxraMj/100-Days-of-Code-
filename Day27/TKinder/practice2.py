38c5c6613a93b90261fd1988a1ee1a0fa84f6908
AxraMj <offxaxra@gmail.com>
2025-01-17 13:02:37 +0530

    Day 27 TKinder button

---
+++ Day27/TKinder/practice2.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+window = Tk()
+window.title("Practice program")
+window.minsize(width=500,height=300)
+
+my_label=Label(text="Click me")
+my_label.pack()
+
+def click_me():
+    my_label["text"]="oh!! I am Clicked"
+    
+
+my_button=Button(text="Click me", command=click_me)
+my_button.pack()
+
+
+window.mainloop()

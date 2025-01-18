2716f0c5f80c493f5b7accb2da6bd813d499dd0f
AxraMj <offxaxra@gmail.com>
2025-01-18 23:16:25 +0530

    Day 28 pomodoro ui

--- Day28/002 pomodoro-start/main.py
+++ Day28/002 pomodoro-start/main.py
@@ -19,14 +19,25 @@ LONG_BREAK_MIN = 20
 
 windows =Tk()
 windows.title("Pomodoro")
-windows.config(padx=100,pady=50,bg=GREEN)
+windows.config(padx=100,pady=50,bg=YELLOW)
+
+#Timer Label
+Timer_label= Label(text="Timer", bg=YELLOW , fg=GREEN, font=(FONT_NAME,35,"bold"))
+Timer_label.grid(column=1,row=0)
+
+#Reset and start button
+start_button=Button(text="Start")
+start_button.grid(column=0,row=2)
+
+Reset_button=Button(text="Reset")
+Reset_button.grid(column=2,row=2)
 
 #displaying image we need photoimage class for image path and canvas class for displaying
-canvas= Canvas(width=200,height=224,bg=GREEN,highlightthickness=0)
+canvas= Canvas(width=200,height=224,bg=YELLOW,highlightthickness=0)
 photo_path=PhotoImage(file="E:/100 Days Python/100-Days-of-Code-/Day28/002 pomodoro-start/tomato.png")
 canvas.create_image(100,112,image=photo_path)
 
 #adding text in picture
 canvas.create_text(100,130,text="00:00" , fill="white", font=(FONT_NAME,35,"bold"))
-canvas.pack()
+canvas.grid(column=1,row=1)
 windows.mainloop()

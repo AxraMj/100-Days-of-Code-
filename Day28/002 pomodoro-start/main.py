9942f7a1d1329ebe7a2792530d346a17b5adc49b
AxraMj <offxaxra@gmail.com>
2025-01-18 23:06:24 +0530

    Day 28 pomodoro

--- Day28/002 pomodoro-start/main.py
+++ Day28/002 pomodoro-start/main.py
@@ -19,12 +19,14 @@ LONG_BREAK_MIN = 20
 
 windows =Tk()
 windows.title("Pomodoro")
-windows.minsize(width=500,height=300)
+windows.config(padx=100,pady=50,bg=GREEN)
 
 #displaying image we need photoimage class for image path and canvas class for displaying
-canvas= Canvas(width=200,height=224)
-image_path="E:/100 Days Python/100-Days-of-Code-/Day28/002 pomodoro-start/tomato.png"
-photo_path=PhotoImage(file=image_path)
+canvas= Canvas(width=200,height=224,bg=GREEN,highlightthickness=0)
+photo_path=PhotoImage(file="E:/100 Days Python/100-Days-of-Code-/Day28/002 pomodoro-start/tomato.png")
 canvas.create_image(100,112,image=photo_path)
+
+#adding text in picture
+canvas.create_text(100,130,text="00:00" , fill="white", font=(FONT_NAME,35,"bold"))
 canvas.pack()
 windows.mainloop()

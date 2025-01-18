481ddd56f3ac97a3df04f3c5803b0767d34d7846
AxraMj <offxaxra@gmail.com>
2025-01-18 22:54:09 +0530

    Day 28 pomodoro

--- Day28/002 pomodoro-start/main.py
+++ Day28/002 pomodoro-start/main.py
@@ -20,4 +20,11 @@ LONG_BREAK_MIN = 20
 windows =Tk()
 windows.title("Pomodoro")
 windows.minsize(width=500,height=300)
+
+#displaying image we need photoimage class for image path and canvas class for displaying
+canvas= Canvas(width=200,height=224)
+image_path="E:/100 Days Python/100-Days-of-Code-/Day28/002 pomodoro-start/tomato.png"
+photo_path=PhotoImage(file=image_path)
+canvas.create_image(100,112,image=photo_path)
+canvas.pack()
 windows.mainloop()

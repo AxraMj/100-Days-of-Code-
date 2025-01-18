1d4ac673c250816af0bcf3f47da5e7103d5a80cd
AxraMj <offxaxra@gmail.com>
2025-01-19 00:04:57 +0530

    Day 28 pomodoro ful completed code

--- Day28/002 pomodoro-start/main.py
+++ Day28/002 pomodoro-start/main.py
@@ -9,22 +9,57 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps=0
+timer = None
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    windows.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    Timer_label.config(text="Timer")
+    check_mark_label.config(text="")
+    global reps
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5*60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        Timer_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        Timer_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        Timer_label.config(text="Work", fg=GREEN)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    count_min=math.floor(count/60)
-    count_sec=count % 60
-    if count_sec < 10:
-        count_sec= f"0{count_sec}"
 
-    canvas.itemconfig(timer_text,text=f"{count_min}:{count_sec}")
-    if count>0:
-        windows.after(1000, count_down, count -1)
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = windows.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_mark_label.config(text=marks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -41,7 +76,7 @@ Timer_label.grid(column=1,row=0)
 start_button=Button(text="Start",command=start_timer)
 start_button.grid(column=0,row=2)
 
-Reset_button=Button(text="Reset")
+Reset_button=Button(text="Reset",command=reset_timer)
 Reset_button.grid(column=2,row=2)
 
 
@@ -51,7 +86,7 @@ photo_path=PhotoImage(file="E:/100 Days Python/100-Days-of-Code-/Day28/002 pomod
 canvas.create_image(100,112,image=photo_path)
 
 #check mark
-check_mark_label=Label(text="✔️", fg=GREEN, bg=YELLOW)
+check_mark_label=Label(fg=GREEN, bg=YELLOW)
 check_mark_label.grid(column=1,row=3)
 
 #adding text in picture

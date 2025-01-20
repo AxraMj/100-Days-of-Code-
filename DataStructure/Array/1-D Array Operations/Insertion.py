e1ca3929a73eb10457c276d31e1d1754fccea6f8
AxraMj <offxaxra@gmail.com>
2025-01-20 14:33:16 +0530

    DS Array insertion and traversal

---
+++ DataStructure/Array/1-D Array Operations/Insertion.py
@@ -0,0 +1,10 @@
+from array import *
+
+arr1=array("i",[20,30,40,50])
+arr1.insert(4,90) #4 is position and 90 is element
+print(arr1)
+
+arr2=array("i",[55,66,77,99])
+#insert 88 in 3rd possition
+arr2.insert(3,88)
+print(arr2)
---
+++ DataStructure/Array/1-D Array Operations/Traversl.py
@@ -0,0 +1,5 @@
+from array import *
+
+arr1=array("i",[10,20,30,40,50])
+for i in arr1:
+    print(i)

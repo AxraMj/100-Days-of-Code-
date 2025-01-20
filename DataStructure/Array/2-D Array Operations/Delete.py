93488d61819438da970a5226f8809755f2a83b4a
AxraMj <offxaxra@gmail.com>
2025-01-20 15:58:47 +0530

    DS 2d Array delete

--- DataStructure/Array/2-D Array Operations/Delete.py
+++ DataStructure/Array/2-D Array Operations/Delete.py
@@ -2,5 +2,5 @@ import numpy as np
 
 new_2darray=np.array([[1,2,3], [4,5,6], [7,8,9]])
 print(new_2darray)
-delete_array=np.delete(new_2darray,0,axis=0)
+delete_array=np.delete(new_2darray,0,axis=1) #axist 0 means row and 1 means column
 print(delete_array)

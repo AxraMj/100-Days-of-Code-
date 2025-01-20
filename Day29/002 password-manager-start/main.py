fc559665efdab5b0aaafa9c2d534a12f57ba372e
AxraMj <offxaxra@gmail.com>
2025-01-20 19:30:17 +0530

    Day29 field Validation

--- Day29/002 password-manager-start/main.py
+++ Day29/002 password-manager-start/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox #meassage box is not class
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -7,15 +8,19 @@ def save():
     email=email_entry.get()
     password=password_entry.get()
 
-    file_path="Day29/002 password-manager-start/data.txt"
-    with open(file_path,"a") as data_file:
-        data_file.write(f"{website} | {email} | {password} \n")
-        website_entry.delete(0,END)
-        password_entry.delete(0,END)
-
-
-        
-
+    if len(website)==0 or len(password)==0:
+        messagebox.showinfo(title="Opps!!", message="Fill all the field")
+    else:
+        #create a message box
+        # messagebox.showinfo(title="title",message="Message")
+        is_ok=messagebox.askokcancel(title=website,message=f"These are the details entered: \nEmail: {email}\n password: {password}\n is it ok to save?")
+        if is_ok:
+            file_path="Day29/002 password-manager-start/data.txt"
+            with open(file_path,"a") as data_file:
+                data_file.write(f"{website} | {email} | {password} \n")
+                website_entry.delete(0,END)
+                password_entry.delete(0,END)
+  
 
 
 # ---------------------------- UI SETUP ------------------------------- #

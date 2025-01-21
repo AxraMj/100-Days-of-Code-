892f9c731c771dd11b79e940163c59fdf945d706
AxraMj <offxaxra@gmail.com>
2025-01-21 20:55:44 +0530

    Day 30 Some errors

---
+++ Day30/Error/index.py
@@ -0,0 +1,17 @@
+#lets try to open a not-exist file
+
+#FileNotFoundError:
+# with open("file1.txt") as file1:
+#     file1.read()
+
+
+#KeyError
+# dic={'a':1, 'b':2}
+# print(dic['c'])
+
+#IndexError
+# list=[10,20,30]
+# print(list[5])
+
+text="abc"
+print(text +5)

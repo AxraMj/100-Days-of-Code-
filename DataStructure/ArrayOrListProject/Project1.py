121eafb66ca9e565f2531b3de14c558731a3c2bd
AxraMj <offxaxra@gmail.com>
2025-01-21 14:13:11 +0530

    DS List/Array project 1st step

--- DataStructure/ArrayOrListProject/Project1.py
+++ DataStructure/ArrayOrListProject/Project1.py
@@ -1,1 +1,13 @@
-#
+#Find Number of days above Average Temperature
+
+
+
+days_temp=int(input("How many day's temperature? "))
+total=0
+for i in range(1,days_temp+1):
+    days=int(input(f"day {i}'s high temp: "))
+    total+=days
+
+avg= round(total/days_temp)
+print(f"Average= {avg}")
+    

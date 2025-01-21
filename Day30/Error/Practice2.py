f2282247730b6341cd872a7d74fb48406f48424a
AxraMj <offxaxra@gmail.com>
2025-01-21 21:53:25 +0530

    Day 30 Practice2

---
+++ Day30/Error/Practice2.py
@@ -0,0 +1,17 @@
+facebook_posts=[
+    {'likes':21,'comments':2},
+    {'likes':13,'comments':2,'share':1},
+    {'likes':33,'comments':8,'share':3},
+    {'comments':4,'share':2},
+    {'comments':1,'share':1},
+    {'likes':19,'comments':3}
+]
+
+total_likes=0
+for post in facebook_posts:
+    try:
+        total_likes=total_likes+post['likes']
+    except KeyError:
+        pass
+
+print(total_likes)

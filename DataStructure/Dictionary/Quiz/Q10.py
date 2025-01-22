321678e082a8a0f7d4768b868bb7e7e72355128b
AxraMj <offxaxra@gmail.com>
2025-01-22 15:45:49 +0530

    DS

--- DataStructure/Dictionary/Quiz/Q10.py
+++ DataStructure/Dictionary/Quiz/Q10.py
@@ -6,7 +6,7 @@ del rec
 rec = {"Name" : "Python", "Age":"20", "Addr" : "NJ", "Country" : "USA"}
 id2 = id(rec)
 print(id1 == id2)
-# A. True
+# A. True ✔️
 # B. False
 # C. 1
 # D. Exception
---
+++ DataStructure/Tuples/Programs/CreateAccess.py
@@ -0,0 +1,12 @@
+#  How to create a Tuple?
+
+t=(1,2,3,4,5)
+print(t)
+# (1, 2, 3, 4, 5)
+
+t1=tuple("abcde")
+print(t1)
+# ('a', 'b', 'c', 'd', 'e')
+
+# Access Tuple elements
+print(t1[0])

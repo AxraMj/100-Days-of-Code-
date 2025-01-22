b249b342900d5fe06f50a0e2adbb9a966ad8fcba
AxraMj <offxaxra@gmail.com>
2025-01-22 16:00:13 +0530

    DS Tuple quiz

--- DataStructure/Tuples/Quiz/Q7.py
+++ DataStructure/Tuples/Quiz/Q7.py
@@ -2,7 +2,7 @@
 # Q-7. What will be the output of the following code block?
 
 l = [1, 2, 3]
-init_tuple = ('Python',) * (l.__len__() - l[::-1][0])
+init_tuple = ('Python',) * (l.__len__() - l[::-1][0]) 
 
 print(init_tuple)
 # A. ()
---
+++ DataStructure/Tuples/Quiz/index.py
@@ -0,0 +1,3 @@
+
+init_tuple = (1,) * 3
+print(init_tuple)

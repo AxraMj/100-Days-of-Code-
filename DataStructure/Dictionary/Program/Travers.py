694495960ec0d9b398a355ba64a8b775211a09f6
AxraMj <offxaxra@gmail.com>
2025-01-22 15:16:29 +0530

    DS Dictionary programs

---
+++ DataStructure/Dictionary/Program/Travers.py
@@ -0,0 +1,4 @@
+dic={'name': 'Alice', 'age': 25, 'city': 'New York'}
+
+for key in dic:
+    print(key, dic[key])
---
+++ DataStructure/Dictionary/Program/Update_Add.py
@@ -0,0 +1,11 @@
+#  Update / add an element to the dictionary
+
+dic={'name': 'Alice', 'age': 25, 'city': 'New York'}
+dic['name']="Akshara"
+print(dic)
+
+# {'name': 'Akshara', 'age': 25, 'city': 'New York'}
+
+#add
+dic['location']='USA'
+print(dic)

2a38a2c28986e00c66b755232e3176ac1630eb0f
AxraMj <offxaxra@gmail.com>
2025-01-22 15:52:38 +0530

    DS Tuple quiz

---
+++ DataStructure/Tuples/Quiz/Q7.py
@@ -0,0 +1,13 @@
+
+# Q-7. What will be the output of the following code block?
+
+l = [1, 2, 3]
+init_tuple = ('Python',) * (l.__len__() - l[::-1][0])
+
+print(init_tuple)
+# A. ()
+# B. (‘Python’)
+# C. (‘Python’, ‘Python’)
+# D. Runtime Exception.
+ 
+

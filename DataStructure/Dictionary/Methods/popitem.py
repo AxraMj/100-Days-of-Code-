5a598c384fd8c211fab457dd35bef55b94be825e
AxraMj <offxaxra@gmail.com>
2025-01-22 12:57:24 +0530

    DS Dictionary methods

--- DataStructure/Dictionary/Methods/popitem.py
+++ DataStructure/Dictionary/Methods/popitem.py
@@ -2,5 +2,6 @@ dic={'name': 'Alice', 'age': 25, 'city': 'New York'}
 print(dic.popitem()) # dict.popitem() takes no arguments
 
 # ('city', 'New York')
+#It removes and returns the last key-value pair as a tuple from the dictionary.
 
 print(dic)
---
+++ DataStructure/Dictionary/Methods/setDefault.py
@@ -0,0 +1,11 @@
+dic={'name': 'Alice', 'age': 25, 'city': 'New York'}
+#dic.setdefault(key,default_value) syntax
+
+
+# The setdefault() method in Python dictionaries is used to retrieve the value of a specified key. 
+# If the key is not present in the dictionary, it inserts the key with a 
+# specified default value and then returns that default value.
+
+dic.setdefault('country', 'USA')
+
+print(dic)

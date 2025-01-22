5ea1cb1d004b28a8b4eb73bf4a10c76cbd222335
AxraMj <offxaxra@gmail.com>
2025-01-22 15:41:26 +0530

    DS Dictionary Quiz

--- DataStructure/Dictionary/Quiz/Q8.py
+++ DataStructure/Dictionary/Quiz/Q8.py
@@ -2,9 +2,9 @@
 
 dict = {'c': 97, 'a': 96, 'b': 98}
 
-for _ in sorted(dict):
-    print (dict[_])
-# A. 96 98 97
+for _ in sorted(dict): #for temp variable we use _
+    print (dict[_]) #sorted using a,b,c
+# A. 96 98 97 ✔️
 # B. 96 97 98
 # C. 98 97 96
 # D. NameError
---
+++ DataStructure/Dictionary/Quiz/Q9.py
@@ -0,0 +1,9 @@
+# Q-9. What will be the output of the following code snippet?
+
+rec = {"Name" : "Python", "Age":"20"}
+r = rec.copy()
+print(id(r) == id(rec))
+# A. True
+# B. False
+# C. 0
+# D. 1

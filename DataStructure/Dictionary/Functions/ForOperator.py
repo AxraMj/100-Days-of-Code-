596d8cc5b428047de774cd5fee6d963550c00a7d
AxraMj <offxaxra@gmail.com>
2025-01-22 13:10:20 +0530

    DS Dictionary for operator

---
+++ DataStructure/Dictionary/Functions/ForOperator.py
@@ -0,0 +1,11 @@
+dic={'name': 'Alice', 'age': 25, 'city': 'New York'}
+
+for i in dic:
+    print(i,dic[i])
+
+# name Alice
+# age 25
+# city New York
+
+for i in range(len(dic)):
+    print(dic[i])
--- DataStructure/Dictionary/Functions/InOperator.py
+++ DataStructure/Dictionary/Functions/InOperator.py
@@ -1,2 +1,4 @@
 dic={'name': 'Alice', 'age': 25, 'city': 'New York'}
 print('name' in dic) #True
+
+ # Checks if the key 'name' exists in the dictionary

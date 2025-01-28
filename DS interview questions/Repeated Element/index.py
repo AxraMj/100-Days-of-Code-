e6726b4593fa6cccf1e9b6fa61795e897f7b8e62
AxraMj <offxaxra@gmail.com>
2025-01-28 20:39:52 +0530

    DS Interview Questions

--- DS interview questions/Repeated Element/index.py
+++ DS interview questions/Repeated Element/index.py
@@ -4,11 +4,11 @@
 #b=[2,1,3,4,5,6,2,1,2]
 #return 2
 
-def function(array):
+def RepeatedElement(array):
     seen=set()
     for i in array:
         if i in seen:
             return i
         seen.add(i)
-array=[2,1,1,3,4,5,6,2,1,2]
-print(function(array))
+array=[3,3,2,1,1,3,4,5,6,2,1,2]
+print(RepeatedElement(array))

0e088c7c30c7f7117e3d47114edd31b17b311536
AxraMj <offxaxra@gmail.com>
2025-01-28 13:10:45 +0530

    DS Interview Questions

--- DS interview questions/TwoSum/index.py
+++ DS interview questions/TwoSum/index.py
@@ -2,7 +2,7 @@
 #array-[1,2,3,5,5] target =10
 
 #brute force approach
-# def sumelement(array,target):
+# def TwoSum(array,target):
 #     for i in range(len(array)):
 #         for j in range(i+1,len(array)):
 #             if array[i] + array[j] == target:
@@ -14,13 +14,27 @@
 
 #better solution
 
-def sumelement(array, target):
-    seen = set()
-    for i in range(len(array)):
-        value = target - array[i]  
-        if value in seen:
-            return f"{value},{array[i]}" 
-        seen.add(array[i])  
+# def TwoSum(array, target):
+#     seen = set()
+#     for i in range(len(array)):
+#         value = target - array[i]  
+#         if value in seen:
+#             return f"{value},{array[i]}" 
+#         seen.add(array[i])  
 
-array = [1, 2, 4, 3, 5]
-print(sumelement(array, 7))  
+# array = [1, 2, 4, 3, 5]
+# print(sumelement(array, 7))  
+
+#write it using class
+class TwoSum:
+     def TwoSum(self,array, target):
+        seen = set()
+        for i in range(len(array)):
+            value = target - array[i]  
+            if value in seen:
+                return f"{value},{array[i]}" 
+            seen.add(array[i])  
+        
+obj=TwoSum()
+array = [2,7,11,15]
+print(obj.TwoSum(array,9))

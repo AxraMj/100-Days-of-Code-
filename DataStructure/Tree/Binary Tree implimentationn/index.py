603a97e3015db95855bc0b4a89b12034ebdd40de
AxraMj <offxaxra@gmail.com>
2025-01-30 12:51:17 +0530

    DS Interview Questions

---
+++ DataStructure/Tree/Binary Tree implimentationn/index.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.left=None
+        self.right=None
+
+class BinaryTree:
+    def __init__(self):
+        self.root=None
+
+    def insert(self,value):
+        newnode=Node(value)
+        if self.root is None:
+            self.root = newnode
+            return
+        current_node=self.root
+        while True:
+            if value < current_node.value:
+                if current_node.left is None:
+                    current_node.left=newnode
+                    return
+                current_node = current_node.left
+            else:
+                if current_node.right is None:
+                    current_node.right = newnode
+                    return
+                current_node = current_node.right
+
+tree = BinaryTree()
+tree.insert(10)
+tree.insert(5)
+tree.insert(15)
